d0b1707698df599ba27807e547422a25e8cf4f4a
Matharea <gorel.paul@gmail.com>
2018-11-13 12:09:42 +0100

    c bon

--- Hanoi.java
+++ Hanoi.java
@@ -4,38 +4,41 @@ public class Hanoi
     {
 	return (tab.contains('1') || (tab.contains('2') && tab.contains('3')));
     }
-    public boolean isValid(String tab, byte i, char c)
+    public boolean isValid(String tab, byte i)
     {
-	if (tab.indexOf(c) < i)
+	if (tab.indexOf(tab.charAt(i)){
 	    return false;
 	return true;
     }
-    public int move(String tab, int i)
+    public Char move(String tab, int i)
 	{
 	    switch tab.charAt(i)
 		{
 			case '1':
 			    if (tab.indexOf('2') < i)
-				return 3;
-			    else
-				return 2;
-				break;
+				return '3';
+			    return '2';
+			    break;
 			case '2':
 			    if (tab.indexOf('3') < i)
-				return 1;
-			    else
-				return 3;
-				break;
+				return '1';
+			    return '3';
+			    break;
 			case '3':
 			    if (tab.indexOf('1') < i)
-				return 2;
-			    else
-				return 1;
-				break;
+				return '2';
+			    return '1';
+			    break;
 		}
     }
     public static void main(String... args)
     {
-	
+	String disk = "11111111";
+	int i = 0;
+	while(isNotDone(disk)){
+	    if (isValid(disk, i))
+		disk.charAt(i) =  move(disk,i);
+	    i++;
+	}
     }
 }
