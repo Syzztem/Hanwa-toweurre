2e1c2e6a588be093bccc6f60032e5a598a293144
Matharea <gorel.paul@gmail.com>
2018-11-13 11:27:08 +0100

    Salut

--- Hanoi.java
+++ Hanoi.java
@@ -1,17 +1,26 @@
 public class Hanoi
 {
-	public boolean isNotDone(String tab)
-	{
-		return (tab.contains('1') || (tab.contains(2) && tab.contains(3)));
+    public boolean isNotDone(String tab)
+    {
+	return (tab.contains('1') || (tab.contains('2') && tab.contains('3')));
+    }
+    public boolean isValid(String tab, byte i, char c)
+    {
+	if (tab.indexOf(c) < i)
+	    return false;
+	return true;
+    }
+    public int move(String tab, int i){
+	if(i == 1){
+	    
+	} else if (i == 2){
+
+	} else {
+
 	}
-	public boolean isValid(String tab, byte i, char c)
-	{
-		if (tab.indexOf(c) < i)
-			return false;
-		return true;
-	}
-	public static void main(String... args)
-	{
-		
-	}
+    }
+    public static void main(String... args)
+    {
+	
+    }
 }
